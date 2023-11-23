82d8f3abda2385e4632f3d213b6dc19bd6c72ba2
Araai <almach3d@gmail.com>
2023-11-24 02:37:11 +0900

    2023/11/24

--- abc317_e.py
+++ abc317_e.py
@@ -3,42 +3,39 @@ from collections import deque
 H, W = map(int, input().split())
 A = [list(input()) for _ in range(H)]
 
-S = set()
+visited = [[-1] * W for _ in range(H)]
 
 for i in range(H):
     for j in range(W):
+        if A[i][j] == '.':
+            continue
         if A[i][j] == 'S':
             start = (i, j)
-        if A[i][j] == 'G':
+        elif A[i][j] == 'G':
             goal = (i, j)
-        if A[i][j] == '#':
-            S.add((i, j))
-        if A[i][j] == '>':
-            S.add((i, j))
-            for k in range(1,W):
-                if j + k >= W or A[i][j+k] != '.':
-                    break
-                S.add((i, j+k))
-        if A[i][j] == 'v':
-            S.add((i, j))
-            for k in range(1,H):
-                if i + k >= H or A[i+k][j] != '.':
-                    break
-                S.add((i+k, j))
-        if A[i][j] == '<':
-            S.add((i, j))
-            for k in range(1,W):
-                if j - k < 0 or A[i][j-k] != '.':
-                    break
-                S.add((i, j-k))
-        if A[i][j] == '^':
-            S.add((i, j))
-            for k in range(1,W):
-                if i - k < 0 or A[i-k][j] != '.':
-                    break
-                S.add((i-k, j))
+        else:
+            visited[i][j] = -2
+            if A[i][j] == '>':
+                for k in range(1,W):
+                    if j + k >= W or A[i][j+k] != '.':
+                        break
+                    visited[i][j+k] = -2
+            elif A[i][j] == 'v':
+                for k in range(1,H):
+                    if i + k >= H or A[i+k][j] != '.':
+                        break
+                    visited[i+k][j] = -2
+            elif A[i][j] == '<':
+                for k in range(1,W):
+                    if j - k < 0 or A[i][j-k] != '.':
+                        break
+                    visited[i][j-k] = -2
+            elif A[i][j] == '^':
+                for k in range(1,W):
+                    if i - k < 0 or A[i-k][j] != '.':
+                        break
+                    visited[i-k][j] = -2
 
-visited = [[-1] * W for _ in range(H)]
 visited[start[0]][start[1]] = 0
 
 q = deque([start])
@@ -50,7 +47,7 @@ while q:
         h, w = x + i, y + j
         if h == -1 or h == H or w == -1 or w == W:
             continue
-        if (h, w) in S:
+        if visited[h][w] == -2:
             continue
         if visited[h][w] >= 0:
             continue
