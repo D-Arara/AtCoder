821833187801e6b282f23ad1afdcf434181c0cea
Araai <almach3d@gmail.com>
2023-12-01 02:48:38 +0900

    2023/12/01

--- abc315_e.py
+++ abc315_e.py
@@ -3,10 +3,7 @@ B = [[]] + [list(map(int, input().split())) for _ in range(N)]
 graph = [[] for _ in range(N+1)]
 
 L = [0] * (N+1)
-<<<<<<< HEAD
-=======
 res = set()
->>>>>>> c7c4924a70d22b2a5a76979aba9b29d15aaf7a91
 for i in range(1,N+1):
     L[i] = B[i][0]
     if L[i] != 0:
@@ -14,10 +11,6 @@ for i in range(1,N+1):
             graph[j].append(i)
 
 q = [1]
-<<<<<<< HEAD
-res = set()
-=======
->>>>>>> c7c4924a70d22b2a5a76979aba9b29d15aaf7a91
 visited = set([1])
 while q:
     u = q.pop()
@@ -36,11 +29,7 @@ while q:
     u = q.pop()
     for v in graph[u]:
         L[v] -= 1
-<<<<<<< HEAD
-        if L[v] == 0:
-=======
         if L[v] == 0 and v in visited:
->>>>>>> c7c4924a70d22b2a5a76979aba9b29d15aaf7a91
             q.append(v)
             res.append(v)
             
