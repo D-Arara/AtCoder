8290c1bbe4aba47692f4fc0a2b0a88a315c9ba59
Araai <almach3d@gmail.com>
2023-12-05 02:53:28 +0900

    2023/12/05

--- abc309_f.py
+++ abc309_f.py
@@ -1,20 +1,38 @@
 N = int(input())
 
 from collections import defaultdict
-H = defaultdict(list)
-W = defaultdict(int)
-D = defaultdict(int)
+H = set()
+W = set()
+D = set()
 Box = []
 for i in range(N):
-    h, w, d = map(int, input().split())
-    H[h].append((w, d))
-    W[w] += 1
-    D[d] += 1
+    h, w, d = sorted(map(int, input().split()))
+    H.add(h)
+    W.add(w)
+    D.add(d)
     Box.append((h, w, d))
-    
-Box.sort()
-w = max(W.keys())
-d = max(D.keys())
-for i in sorted(H.keys()):
-    print(i)
 
+Box.sort()
+H = sorted(H)
+H = dict(zip(H, range(len(H))))
+W = sorted(W)
+W = dict(zip(W, range(len(W))))
+D = sorted(D)
+D = dict(zip(D, range(len(D))))
+for i in range(N):
+    Box[i] = (H[Box[i][0]], W[Box[i][1]], D[Box[i][2]])
+
+DD = defaultdict(int)
+for i in range(N):
+    DD[Box[i][1]] = max(DD[Box[i][1]], Box[i][2])
+# print(Box)
+# print(DD)
+for i in range(N):
+    w = Box[i][1]
+    d = Box[i][2]
+    for j in range(w+1,len(W)):
+        # print(i, j)
+        if d < DD[j]:
+            print('Yes')
+            exit()
+print('No')
